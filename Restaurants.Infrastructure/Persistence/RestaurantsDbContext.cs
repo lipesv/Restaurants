acbbb7e40e74991d87251c7f9420d7cdc0738851
lipesv <fellipe.svieira@gmail.com>
2026-01-15 10:54:23 -0300

    Setting up Infrastructure for module creation and initial configuration using Entity Framework.

---
+++ Restaurants.Infrastructure/Persistence/RestaurantsDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Restaurants.Domain.Entities;
+
+namespace Restaurants.Infrastructure.Persistence;
+
+internal class RestaurantsDbContext : DbContext
+{
+    internal DbSet<Restaurant> Restaurants { get; set; }
+    internal DbSet<Dish> Dishes { get; set; }
+}

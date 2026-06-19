80fb358ba14e8695d0eb40012f7830bcf578d683
Fellipe da Silva Vieira <fellipe.svieira@gmail.com>
2026-06-19 17:56:29 -0300

    Change client to authenticated for restaurant tests

--- tests/Restaurants.API.Tests/Controllers/RestaurantsControllerTests.cs
+++ tests/Restaurants.API.Tests/Controllers/RestaurantsControllerTests.cs
@@ -152,7 +152,7 @@ public class RestaurantsControllerTests : IClassFixture<WebApplicationFactory<Pr
         // Arrange
         const int id = 1;
 
-        var client = CreateAnonymousClient();
+        var client = CreateAuthenticatedClient();
 
         // Act
         var response = await client.GetAsync($"/api/restaurants/{id}");
